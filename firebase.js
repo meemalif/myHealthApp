3504c5960a30f0b838240e2392e5abdd3add7b87
Muneeb Ahmad <muahmad.bscs21seecs@seecs.edu.pk>
2024-04-09 03:02:44 +0500

    initial environment setup with firebase

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBq2y40HpCohUqJhgwTU9m9bDWMh_RmFvI",
+  authDomain: "healthkit-b1940.firebaseapp.com",
+  projectId: "healthkit-b1940",
+  storageBucket: "healthkit-b1940.appspot.com",
+  messagingSenderId: "638553398255",
+  appId: "1:638553398255:web:77f5f5d3a580a101f5b41a",
+  measurementId: "G-K6T86EBL1H",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+export const storage = getStorage(app);
+export const firestore = getFirestore(app);
